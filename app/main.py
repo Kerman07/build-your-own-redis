e8be6f5c616881b91db4fdaa22447a5f75b70410
Kerim Mandzo <kerim.mandzo@klika.ba>
2023-08-26 18:33:27 +0200

    first messy version that passes echo tests

--- app/main.py
+++ app/main.py
@@ -6,17 +6,27 @@ PORT = 6379
 NUM_THREADS = 5
 
 
+class RESP:
+    @staticmethod
+    def resp_single_string(string):
+        return bytes(f"+{string}\r\n", "utf-8")
+
+
 def accept_connection(conn, addr):
     with conn:
         print(f"Connected by {addr}")
-        while data := conn.recv(1024):
-            print("Received")
-            conn.send(b"+PONG\r\n")
+        while data := conn.recv(1024).decode("utf-8"):
+            print("Received: ")
+            print(data.replace("\r\n", " "))
+            if "\r\necho" in data:
+                word = data.split("\r\n")[-2]
+                conn.send(RESP.resp_single_string(word))
+            else:
+                conn.send(RESP.resp_single_string("PONG"))
 
 
 def main():
     server_socket = socket.create_server((HOST, PORT), reuse_port=True)
-    server_socket.listen()
 
     with ThreadPoolExecutor(max_workers=NUM_THREADS) as executor:
         while True:
