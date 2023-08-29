acce34358fad9c76736de2bcd91b621b7bfc7f9e
Kerim Mandzo <kerim.mandzo@klika.ba>
2023-08-29 16:43:48 +0200

    code passes expiry tests

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import socket
 from concurrent.futures import ThreadPoolExecutor
+from datetime import datetime, timedelta
 
 HOST = "localhost"
 PORT = 6379
@@ -10,6 +11,10 @@ class RESP:
     @staticmethod
     def resp_single_string(string):
         return bytes(f"+{string}\r\n", "utf-8")
+
+    @staticmethod
+    def null_bulk_string():
+        return bytes("$-1\r\n", "utf-8")
 
 
 def accept_connection(conn, addr):
@@ -22,14 +27,26 @@ def accept_connection(conn, addr):
                 word = data.split("\r\n")[-2]
                 conn.send(RESP.resp_single_string(word))
             elif "\r\nset" in data:
-                key, val = data.split("\r\n")[-4], data.split("\r\n")[-2]
-                print(key, val)
-                memory[key] = val
+                if "\r\npx" in data:
+                    value, time_delta = data.split("\r\npx")
+                    expiry = time_delta.split("\r\n")[-2]
+                    key, val = value.split("\r\n")[-4], value.split("\r\n")[-2]
+                    print(f"{key} - {val} - {expiry}")
+                    memory[key] = {
+                        "value": val,
+                        "expires": datetime.now()
+                        + timedelta(microseconds=int(expiry) * 1000),
+                    }
+                else:
+                    key, val = data.split("\r\n")[-4], data.split("\r\n")[-2]
+                    memory[key] = {"value": val, "expires": None}
                 conn.send(RESP.resp_single_string("OK"))
             elif "\r\nget" in data:
                 key = data.split("\r\n")[-2]
-                print(key)
-                conn.send(RESP.resp_single_string(memory[key]))
+                if memory[key]["expires"] and datetime.now() > memory[key]["expires"]:
+                    conn.send(RESP.null_bulk_string())
+                else:
+                    conn.send(RESP.resp_single_string(memory[key]["value"]))
             else:
                 conn.send(RESP.resp_single_string("PONG"))
 
