9894257328d05dd24883467d801129a61878b3f7
Kerim Mandzo <kerim.mandzo@klika.ba>
2023-09-09 13:52:52 +0200

    refactor some parts

--- app/main.py
+++ app/main.py
@@ -10,11 +10,15 @@ NUM_THREADS = 5
 class RESP:
     @staticmethod
     def resp_single_string(string):
-        return bytes(f"+{string}\r\n", "utf-8")
+        return f"+{string}\r\n".encode("utf-8")
 
     @staticmethod
     def null_bulk_string():
-        return bytes("$-1\r\n", "utf-8")
+        return "$-1\r\n".encode("utf-8")
+
+
+def calculate_expiration_time(expiry):
+    return datetime.now() + timedelta(microseconds=int(expiry) * 1000)
 
 
 def accept_connection(conn, addr):
@@ -23,32 +27,35 @@ def accept_connection(conn, addr):
         while data := conn.recv(1024).decode("utf-8"):
             print("Received: ")
             print(data.replace("\r\n", " "))
-            if "\r\necho" in data:
-                word = data.split("\r\n")[-2]
-                conn.send(RESP.resp_single_string(word))
-            elif "\r\nset" in data:
-                if "\r\npx" in data:
-                    value, time_delta = data.split("\r\npx")
-                    expiry = time_delta.split("\r\n")[-2]
-                    key, val = value.split("\r\n")[-4], value.split("\r\n")[-2]
-                    print(f"{key} - {val} - {expiry}")
-                    memory[key] = {
-                        "value": val,
-                        "expires": datetime.now()
-                        + timedelta(microseconds=int(expiry) * 1000),
-                    }
-                else:
-                    key, val = data.split("\r\n")[-4], data.split("\r\n")[-2]
-                    memory[key] = {"value": val, "expires": None}
-                conn.send(RESP.resp_single_string("OK"))
-            elif "\r\nget" in data:
-                key = data.split("\r\n")[-2]
-                if memory[key]["expires"] and datetime.now() > memory[key]["expires"]:
-                    conn.send(RESP.null_bulk_string())
-                else:
-                    conn.send(RESP.resp_single_string(memory[key]["value"]))
-            else:
-                conn.send(RESP.resp_single_string("PONG"))
+            command = data.split("\r\n")[2]
+            match command:
+                case "echo":
+                    word = data.split("\r\n")[-2]
+                    conn.send(RESP.resp_single_string(word))
+                case "set":
+                    if "\r\npx" in data:
+                        value, time_delta = data.split("\r\npx")
+                        expiry = time_delta.split("\r\n")[-2]
+                        key, val = value.split("\r\n")[-4], value.split("\r\n")[-2]
+                        memory[key] = {
+                            "value": val,
+                            "expires": calculate_expiration_time(expiry),
+                        }
+                    else:
+                        key, val = data.split("\r\n")[-4], data.split("\r\n")[-2]
+                        memory[key] = {"value": val, "expires": None}
+                    conn.send(RESP.resp_single_string("OK"))
+                case "get":
+                    key = data.split("\r\n")[-2]
+                    if key not in memory or (
+                        memory[key]["expires"]
+                        and datetime.now() > memory[key]["expires"]
+                    ):
+                        conn.send(RESP.null_bulk_string())
+                    else:
+                        conn.send(RESP.resp_single_string(memory[key]["value"]))
+                case _:
+                    conn.send(RESP.resp_single_string("PONG"))
 
 
 def main():
